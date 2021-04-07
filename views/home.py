9bbf50b041363ce61dc6138ada5b45007be65779
Mohamed Reda <mohamed@wrangleapp.io>
2021-04-07 17:39:15 +0200

    sending data as a report

--- views/home.py
+++ views/home.py
@@ -2,15 +2,20 @@ import fastapi
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
+from services import report_service
+
 templates = Jinja2Templates('templates')
 router = fastapi.APIRouter()
 
 
-@router.get('/')
-def index(request: Request):
-    return templates.TemplateResponse('home/index.html', {'request': request})
+@router.get('/', include_in_schema=False)
+async def index(request: Request):
+    events = await report_service.get_reports()
+    data = {'request': request, 'events': events}
+
+    return templates.TemplateResponse('home/index.html', data)
 
 
-@router.get('/favicon.ico')
+@router.get('/favicon.ico', include_in_schema=False)
 def favicon():
     return fastapi.responses.RedirectResponse(url='/static/img/favicon.ico')
